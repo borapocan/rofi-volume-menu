f2b11c3c9fcf35f80648a0d51cad6eaf52429262
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2022-10-27 09:06:57 +0300

    Add files via upload

---
+++ volume.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+done=0;
+
+#while [[ $terminate != "finish" ]]; do
+while [[ $done != 1 ]]; do
+
+  #if [[ "$done" -ne 0]]; then
+   # break
+  #fi
+  
+  volume=$(pamixer --get-volume);
+  
+  chosen=$(printf "Mute\nMax\n+10\n-10\nExit" | rofi -dmenu -i -p "$volume" -theme-str '@import "nord.rasi"');
+  
+  case $chosen in
+  
+    "Mute") pactl set-sink-volume 0 0;
+  
+    ;;
+  
+    "Max") pactl set-sink-volume 0 +250%;
+  
+    ;;
+  
+    "+10") pactl set-sink-volume 0 +10%;
+  
+    ;;
+  
+    "-10") pactl set-sink-volume 0 -10%;
+  
+    ;;
+    
+    "Exit") done=1;
+
+    ;;
+  
+  esac
+
+done
